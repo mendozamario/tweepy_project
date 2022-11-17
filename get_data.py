018882ed50ee32f620b34b55280f624c2d3be9be
mendozamario <menriquemendoza@unicesar.edu.co>
2022-11-17 11:53:36 -0500

    Solutioned Fix

--- get_data.py
+++ get_data.py
@@ -24,38 +24,6 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 myConnection = mysql.connector.connect(host='localhost', user=username, passwd=password, database='mys')
 
-def saveUser(username) :
-    user = visualizeUsers(username)
-    name=""
-    screen_name=""
-    location=""
-    id=0
-    friends_count=0
-    followers_count=0
-    listed_count=0
-    favourites_count=0
-    statuses_count=0
-    verfied=False
-
-    id=int(user["id"])
-    name=str(user["name"])
-    screen_name=str(user["screen_name"])
-    location=str(user["location"])
-    followers_count=int(user["followers_count"])
-    friends_count=int(user["friends_count"])
-    listed_count=int(user["listed_count"])
-    favourites_count=int(user["favourites_count"])
-    statuses_count=int(user["statuses_count"])
-    verfied=bool(user["verified"])
-
-    myCursor = myConnection.cursor()
-    sql = "INSERT INTO usuario (id, username, screen_name, location, followers_count, friends_count, listed_count, favourites_count, statuses_count, verified) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (id, name, screen_name, location, followers_count, friends_count, listed_count, favourites_count, statuses_count, verfied)
-    myCursor.execute(sql, values)
-
-    myConnection.commit()
-    print("1 record inseted, ID:", myCursor.lastrowid)
-
 def getUsers() :
     myCursor = myConnection.cursor()
     myCursor.execute("SELECT * FROM usuario")
@@ -65,7 +33,19 @@ def getUsers() :
         print(x)
 
 def visualizeUsers(username) :
-    user = api.get_user(screen_name = username)
+    return api.get_user(screen_name = username)
 
-saveUser("tokischa_")
+def saveUser(username) :
+    user = visualizeUsers(username)
+
+    myCursor = myConnection.cursor()
+    sql = "INSERT INTO usuario (id, username, screen_name, location, followers_count, friends_count, listed_count, favourites_count, statuses_count, verified) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (user["id_str"], user["name"], user["screen_name"], user["location"], user["followers_count"], user["friends_count"], user["listed_count"], user["favourites_count"], user["statuses_count"], user["verified"])
+    myCursor.execute(sql, values)
+
+    myConnection.commit()
+    print("1 record inseted, ID:", myCursor.lastrowid)
+
+
+saveUser("eduercalvo")
     
